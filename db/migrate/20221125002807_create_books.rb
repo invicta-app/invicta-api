eb827b470c40fdbfd51ef12b3ad6df95bf494e7e
marcelo-mantilla <marcelo.mantilla@appcabinet.com>
2022-12-14 12:49:35 -0500

    rename migration files to follow rails conventions

--- db/migrate/20221125002807_create_books.rb
+++ db/migrate/20221125002807_create_books.rb
@@ -1,4 +1,4 @@
-class CreateVolumes < ActiveRecord::Migration[7.0]
+class CreateBooks < ActiveRecord::Migration[7.0]
   def change
     create_table :books, id: :uuid do |t|
       t.string :title, null: false
--- db/migrate/20221125075804_create_book_sections.rb
+++ db/migrate/20221125075804_create_book_sections.rb
@@ -1,7 +1,7 @@
-class CreateSections < ActiveRecord::Migration[7.0]
+class CreateBookSections < ActiveRecord::Migration[7.0]
   def change
     create_table :book_sections, id: :uuid do |t|
-      t.belongs_to :book, type: :uuid, foreign_key: true, null: false # TODO change to book_id
+      t.belongs_to :book, type: :uuid, foreign_key: true, null: false
       t.string :section_title, null: true
       t.integer :sequence, null: true
       t.integer :length, null: false
