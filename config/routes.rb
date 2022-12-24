bd75c744828b280843b6fcf714b0f1f20fed32b6
marcelo-mantilla <marcelo.mantilla@appcabinet.com>
2022-12-24 10:42:03 -0500

    Finalize Sign In Feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+  }
+
   root to: 'books#index'
 
   namespace :api do
